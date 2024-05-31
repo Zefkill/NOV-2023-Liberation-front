4f3fc457b4073920e5302379ec7c3875060e762d
Haktan <h.yilmaz.cor@gmail.com>
2024-05-31 23:33:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ let contact = document.getElementById("contact");
 let hobby = document.getElementById("hobbies");
 let language = document.getElementById("talen");
 let education = document.getElementById("onderwijs");
+let project = document.getElementById("projecten");
 let qwerty = document.getElementById("qwerty");
 let qwerty3 = document.getElementById("qwerty3");
 let qwerty4 = document.getElementById("qwerty4");
+let qwerty5 = document.getElementById("qwerty5");
 let qwerty6 = document.getElementById("qwerty6");
 
 //Arrays
 let contactList = [
   "+31 649724206", 
-  "h.yilmaz.cor@hotmail.com"
+  "h.yilmaz.cor@gmail.com"
 ];
 
 let hobbyList = [
@@ -22,10 +24,10 @@ let hobbyList = [
   "Auto's spotten",
   "Verdiepen in PC hardware",
 ];
-let talenList = [
-  "Turks", 
+let talenList = [ 
+  "Nederlands",
   "Engels", 
-  "Nederlands"
+  "Turks",
 ];
 
 let educationList = [
@@ -33,6 +35,13 @@ let educationList = [
   "Mavo Schravenlant. Anglia certificate of English ESOL international, Advanced (CEFR B2) level behaald in 2023",
   "Techniek College Rotterdam. Software Developer Niveau 4, Heden",
 ];
+
+let projectList = [
+  "Mijn meest recente project waar ik nog bezig mee ben: https://github.com/Zefkill/Eternal",
+  "Mijn tweede project: https://github.com/Zefkill/APR-2024-Inevitable",
+  "Mijn eerste project: Daar zit u nu in",
+];
+
 
 //Functions
 function showContact() {
@@ -81,6 +90,22 @@ function showTalen() {
     qwerty4.innerHTML = text;
   }
 }
+
+function showProject() {
+  let proLen = projectList.length;
+  let text = "<ul>";
+  for (let i = 0; i < proLen; i++) {
+    text += "<li>" + projectList[i] + "</li>";
+  }
+  text += "</ul>";
+
+  if (qwerty5.innerHTML === text) {
+    qwerty5.innerHTML = "";
+  } else {
+    qwerty5.innerHTML = text;
+  }
+}
+
 
 function showEd() {
   let edLen = educationList.length;
